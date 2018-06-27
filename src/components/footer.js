3eb285efbab799ff2b6abd0222ffe67dfd0aecb9
philiplackmaker <philip@potatolondon.com>
2018-06-27 11:13:52 +0100

    Addeding new files

---
+++ src/components/footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = ({ siteTitle }) => (
+  <div
+    style={{
+      background: 'rebeccapurple',
+      marginBottom: '1.45rem',
+    }}
+  >
+    <div
+      style={{
+        top: '50',
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'Red',
+            textDecoration: 'none',
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+  </div>
+)
+
+export default Footer
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+import Footer from '../components/footer'
+
+
+export default () => <div>Hello world!<Footer/>
+</div>
